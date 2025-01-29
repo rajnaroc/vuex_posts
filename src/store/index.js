59dd0cdcd83d1bbb2452143afb92d417e1a5e661
Aaron Campos <aaroncm611@gmail.com>
2025-01-29 11:01:43 +0100

    acabado los array de post y users

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,53 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    posts: [],
+    users: []
   },
   getters: {
+    allUser(state){
+      return state.users
+    },
+    allPost(state){
+        return state.posts
+    },
+    postByUser(state,id){
+      return  [...state.posts].filter((a => a.userId == id ));
+    }
   },
   mutations: {
+    Set_POST(state,posts){
+      state.posts = posts
+    },
+    Set_USERS(state,users){
+      state.users = users
+    }
   },
   actions: {
+    async getPosts({commit}){
+      try {
+          let respo = await fetch("https://jsonplaceholder.typicode.com/posts")
+          let datos = await respo.json()
+
+          commit("Set_POST",datos)
+        }
+        catch(error){
+            console.log(error);
+  
+        }
+    },
+    async getUsers({commit}){
+      try {
+        let respo = await fetch("https://jsonplaceholder.typicode.com/users")
+        let datos = await respo.json()
+
+        commit("Set_USERS",datos)
+      }
+      catch(error){
+          console.log(error);
+
+      }
+    }
   },
   modules: {
   }
---
+++ src/views/PostView.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="">
+        <h1>Post <I></I>t Friends</h1>
+        <div class="">
+            <label class="">Filtrar por usuario</label>
+            <select class="" id="" v-model="userSelected">
+                <option value="" disabled>Todos</option>
+                <option :value="user.name" v-for="(user, index) in allUsers" :key="index">
+                    {{ user.name }}
+                </option>
+            </select>
+        </div>
+        <!-- lista de notas -->
+        <div class="">
+            <h2 class="">Post its</h2>
+            <div v-for="(post, index) in allPost" :key="index" class="">
+                <h3 class="font-bold text-orange-500">{{ post.title }}</h3>
+                <h3 class="">{{ post.body }}</h3>
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    import { mapActions,mapGetters } from 'vuex'
+export default {
+    name: "PostView",
+    data(){
+        return{
+            userSelected: ""
+        }
+    },
+    computed:{
+    ...mapGetters(["allPost", "allUsers"])
+    },methods:{
+        ...mapActions(["getUsers","getPosts"])
+    },
+    mounted(){
+        this.getPosts()
+        this.getUsers()
+    }
+}
+</script>
+
+<style>
+
+</style>
